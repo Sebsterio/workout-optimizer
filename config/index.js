da298f58a8e827bdb9fbb35b921c3c87381f94aa
Sebsterio <sebastian.rosloniec@gmail.com>
2020-06-06 18:51:05 +0200

    connect MongoDB

--- config/index.js
+++ config/index.js
@@ -3,8 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-	PORT: process.env.PORT,
+	PORT: process.env.PORT || 5000,
 	MONGO_URI: process.env.MONGO_URI,
-	MONGO_DB_NAME: process.env.MONGO_DB_NAME,
 	JWT_SECRET: process.env.JWT_SECRET,
 };
--- server.js
+++ server.js
@@ -1,6 +1,45 @@
-import app from "./app";
+import express from "express";
+import mongoose from "mongoose";
+import path from "path";
+import cors from "cors";
+import morgan from "morgan";
+
 import config from "./config";
 
-const { PORT } = config;
+const { MONGO_URI, PORT } = config;
+
+const app = express();
+
+// -------------- Middleware -------------
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+// ---------------- Mongo ----------------
+
+const db = MONGO_URI;
+
+mongoose
+	.connect(db, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+	}) // Adding new mongo url parser
+	.then(() => console.log("MongoDB Connected..."))
+	.catch((err) => console.log(err));
+
+// --------------- Routing ---------------
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+	// Set static folder
+	app.use(express.static("client/build"));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
+// ---------------------------------------
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
