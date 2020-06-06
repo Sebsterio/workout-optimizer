951ac44caaaabe672ae64bde5b775564f0d9c1d5
Sebsterio <sebastian.rosloniec@gmail.com>
2020-06-06 18:25:50 +0200

    boilerplate server and client

---
+++ app.js
@@ -0,0 +1,39 @@
+import express from "express";
+import mongoose from "mongoose";
+import path from "path";
+import cors from "cors";
+import morgan from "morgan";
+import config from "./config";
+
+const { MONGO_URI, MONGO_DB_NAME } = config;
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+// DB Config
+const db = `${MONGO_URI}/${MONGO_DB_NAME}`;
+
+// Connect to Mongo
+mongoose
+	.connect(db, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+	}) // Adding new mongo url parser
+	.then(() => console.log("MongoDB Connected..."))
+	.catch((err) => console.log(err));
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+	// Set static folder
+	app.use(express.static("client/build"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
+export default app;
