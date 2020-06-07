7cb1fbbe433289dc89d5bc88a9225acc9faf29ce
Sebsterio <sebastian.rosloniec@gmail.com>
2020-06-07 18:35:39 +0200

    add nav links

--- client/src/components/header/header.component.js
+++ client/src/components/header/header.component.js
@@ -1,20 +1,39 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 import "./header.scss";
 
-const Header = () => (
-	<div className="header">
-		<div className="header__wrap">
-			<nav>
-				<div className="nav-item">Schedule</div>
-				<div className="nav-item">Protocols</div>
-				<div className="nav-item">Statistics</div>
-				<div className="nav-item">User</div>
-				<div className="nav-item">Options</div>
-				<div className="nav-item">Expand</div> {/* desktop only */}
-			</nav>
+const Header = (props) => {
+	const { main, protocols, statistics, user, options } = props.routes;
+	const expandView = () => {};
+
+	return (
+		<div className="header">
+			<div className="header__wrap">
+				<nav>
+					<NavLink className="nav-item" to={main}>
+						Schedule
+					</NavLink>
+					<NavLink className="nav-item" to={protocols}>
+						Protocols
+					</NavLink>
+					<NavLink className="nav-item" to={statistics}>
+						Statistics
+					</NavLink>
+					<NavLink className="nav-item" to={user}>
+						User
+					</NavLink>
+					<NavLink className="nav-item" to={options}>
+						Options
+					</NavLink>
+					{/* desktop only */}
+					<div className="nav-item" onClick={expandView}>
+						Expand
+					</div>
+				</nav>
+			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 export default Header;
