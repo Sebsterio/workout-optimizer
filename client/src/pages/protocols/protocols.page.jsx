f321a94b2f2c6fe61f145e08a53ad2d5e6486450
Sebsterio <sebastian.rosloniec@gmail.com>
2020-06-07 18:53:55 +0200

    set up client routing

---
+++ client/src/pages/protocols/protocols.page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProtocolsPage = () => {
+	return <h1>ProtocolsPage</h1>;
+};
+
+export default ProtocolsPage;
---
+++ client/src/pages/tracker/tracker.page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TrackerPage = () => {
+	return <h1>TrackerPage</h1>;
+};
+
+export default TrackerPage;
