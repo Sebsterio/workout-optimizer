7796a6f73f0e806a68bc73a85f3a6d0a18f052ec
Sebsterio <sebastian.rosloniec@gmail.com>
2020-06-07 19:28:09 +0200

    set up client routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 import store from "./redux/store";
 import * as serviceWorker from "./serviceWorker";
 
-import "./index.css";
+import "./index.scss";
 import AppContainer from "./app/app.container";
 
 ReactDOM.render(
 	<Provider store={store}>
-		<AppContainer />
+		<BrowserRouter>
+			<AppContainer />
+		</BrowserRouter>
 	</Provider>,
 	document.getElementById("root")
 );
