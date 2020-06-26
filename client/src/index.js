db467de93eeb2ff67070ed9eff743e06bbea8f2f
Sebsterio <sebastian.rosloniec@gmail.com>
2020-06-26 13:13:30 +0200

    attempt to fix issue with deployment

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter } from "react-router-dom";
 
 import { store, persistor } from "./redux/store";
 // import * as serviceWorker from "./serviceWorker";
--- server/app.js
+++ server/app.js
@@ -48,18 +48,19 @@ app.use("/api/log", logRoutes);
 
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "..", "client", "build")));
+	app.use(express.static(path.join(__dirname, "../client/build")));
 	app.get("*", (req, res) => {
-		res.sendFile(
-			path.resolve(__dirname, "..", "client", "build", "index.html")
-		);
+		res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 	});
 }
 
 // ---------------------------------------
 
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+app.listen(port, (err) => {
+	if (err) throw err;
+	console.log(`Server started on PORT ${port}`);
+});
 
 module.exports = app;
