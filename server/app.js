60324cb136becf8752719859b0ac21e74586b376
Sebsterio <sebastian.rosloniec@gmail.com>
2020-06-26 11:30:48 +0200

    fix bugs causing crash when deployed

--- server/app.js
+++ server/app.js
@@ -45,7 +45,9 @@ app.use("/api/log", logRoutes);
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/build"));
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+		res.sendFile(
+			path.resolve(__dirname, "..", "client", "build", "index.html")
+		);
 	});
 }
 
