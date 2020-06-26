cfbbd67e90a30c50e40314a87846ef77973cc822
Sebsterio <sebastian.rosloniec@gmail.com>
2020-06-26 13:50:49 +0200

    debugging issue with deployment

--- client/src/redux/protocol/protocol.reducer.js
+++ client/src/redux/protocol/protocol.reducer.js
@@ -1,8 +1,10 @@
 // import protocolActionTypes from "./protocol.types";
-// import { getFields } from "./protocol.utils";
 import { INITIAL_STATE } from "./protocol.initialState";
 
 const protocolReducer = (state = INITIAL_STATE, action) => {
+	//
+	console.log("Protocol initial state: ", INITIAL_STATE);
+	//
 	switch (action.type) {
 		default:
 			return state;
--- server/app.js
+++ server/app.js
@@ -48,9 +48,11 @@ app.use("/api/log", logRoutes);
 
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../client/build")));
+	app.use(express.static(path.join(__dirname, "..", "client", "build")));
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+		res.sendFile(
+			path.resolve(__dirname, "..", "client", "build", "index.html")
+		);
 	});
 }
 
