9d3f51b5a18ee9a8c54df7d3f375a1b5669e45a5
Sebsterio <sebastian.rosloniec@gmail.com>
2020-06-28 20:03:19 +0200

    implement "move protocol field"

--- client/src/redux/protocol/protocol.utils.js
+++ client/src/redux/protocol/protocol.utils.js
@@ -1,6 +1,5 @@
 export const getFieldsWithNewMaxCustomRest = (state, payload) => {
 	const { field, rest } = payload;
-
 	if (!rest) return state.fields;
 
 	return state.fields.map((f) => {
@@ -12,7 +11,7 @@ export const getFieldsWithNewMaxCustomRest = (state, payload) => {
 };
 
 export const getUpdatedState = (state, payload) => {
-	const { mode } = payload;
+	const { mode, field, newFieldData, direction } = payload;
 	console.log(payload);
 
 	if (mode === "replace-prop") {
@@ -22,7 +21,6 @@ export const getUpdatedState = (state, payload) => {
 	}
 
 	if (mode === "replace-field") {
-		const { field, newFieldData } = payload;
 		const newFields = state.fields.map((f) => {
 			if (f === field) return { ...newFieldData };
 			return f;
@@ -31,13 +29,11 @@ export const getUpdatedState = (state, payload) => {
 	}
 
 	if (mode === "delete-field") {
-		const { field } = payload;
 		const newFields = state.fields.filter((f) => f !== field);
 		return { fields: newFields };
 	}
 
 	if (mode === "duplicate-field") {
-		const { field } = payload;
 		const index = state.fields.indexOf(field);
 		const newField = {
 			...field,
@@ -45,6 +41,15 @@ export const getUpdatedState = (state, payload) => {
 		};
 		const newFields = [...state.fields];
 		newFields.splice(index, 0, newField);
+		return { fields: newFields };
+	}
+
+	if (mode === "move-field") {
+		const step = direction === "up" ? -1 : direction === "down" ? 1 : 0;
+		const index = state.fields.indexOf(field);
+		const newIndex = index + step;
+		const newFields = [...state.fields];
+		newFields.splice(newIndex, 0, newFields.splice(index, 1)[0]);
 		return { fields: newFields };
 	}
 };
