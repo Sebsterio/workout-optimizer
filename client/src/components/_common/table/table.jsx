4cef0a5419640f24e882e7a6e73c67c609e5a877
Sebsterio <sebastian.rosloniec@gmail.com>
2020-06-30 22:07:30 +0200

    refactor LogForm and  extract common components

---
+++ client/src/components/_common/table/table.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import shortid from "shortid";
+import "./table.scss";
+
+export const TableCell = ({ children, head }) => {
+	return head ? (
+		<th className="table__cell">{children}</th>
+	) : (
+		<td className="table__cell">{children}</td>
+	);
+};
+
+export const Table = ({
+	headCells, // array of cells
+	emptyCornerCell, // add empty left-most head cell
+	bodyRows, // array of rows (row = array of cells)
+}) => (
+	<table className="table">
+		<thead>
+			<tr>
+				{emptyCornerCell && <TableCell head />}
+				{headCells.map((cell) => (
+					<TableCell head key={shortid.generate()}>
+						{cell}
+					</TableCell>
+				))}
+			</tr>
+		</thead>
+		<tbody>
+			{bodyRows.map((row) => (
+				<tr key={shortid.generate()}>
+					{row.map((cell) => (
+						<TableCell head key={shortid.generate()}>
+							{cell}
+						</TableCell>
+					))}
+				</tr>
+			))}
+		</tbody>
+	</table>
+);
