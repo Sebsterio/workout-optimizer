898dbc9275e2a1c7544bff0c652add38a33dffbe
Sebsterio <sebastian.rosloniec@gmail.com>
2020-07-08 16:46:42 +0000

    minor changes

--- client/src/components/field/field.utils.js
+++ client/src/components/field/field.utils.js
@@ -1,4 +1,5 @@
 export const getIntensity = (intensity) => (intensity < 3 ? intensity : "3");
+
 export const getRestLevel = (restLevel) => (restLevel < 3 ? restLevel : "3");
 
 export const getCompletion = (details) => {
