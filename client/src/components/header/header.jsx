9794d83b6a210b1b3bd193365745e7d5399449c6
Sebsterio <sebastian.rosloniec@gmail.com>
2020-10-09 14:21:38 +0200

    add icons and styles in Header

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -7,12 +7,14 @@ const Header = ({ routes }) => {
 	return (
 		<div className="header">
 			<div className="header__wrap">
-				<nav>
+				<div className="header__logo">Logo</div>
+				<nav className="header__nav">
 					{routes
 						.filter((route) => route.inNavBar)
-						.map((route) => (
-							<NavLink className="nav-item" to={route.path} key={route.path}>
-								{route.name}
+						.map(({ path, icon, name }) => (
+							<NavLink className="nav-link" to={path} key={path}>
+								<img className="nav-link__icon" src={icon} alt={name} />
+								<span className="nav-link__text">{name}</span>
 							</NavLink>
 							// add label; hidden except when navLink active
 						))}
--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -6,6 +6,12 @@ import Account from "containers/account";
 
 import { ProgramMenu } from "containers/program-menu";
 
+import trackerIcon from "assets/icons/tracker.svg";
+import programsIcon from "assets/icons/programs.svg";
+import statisticsIcon from "assets/icons/statistics.svg";
+import accountIcon from "assets/icons/account.svg";
+import optionsIcon from "assets/icons/options.svg";
+
 export const routes = [
 	// In nav
 	{
@@ -13,30 +19,35 @@ export const routes = [
 		name: "Tracker",
 		component: Tracker,
 		inNavBar: true,
+		icon: trackerIcon,
 	},
 	{
 		path: "/programs",
 		name: "Programs",
 		component: Programs,
 		inNavBar: true,
+		icon: programsIcon,
 	},
 	{
 		path: "/statistics",
 		name: "Statistics",
 		component: Statistics,
 		inNavBar: true,
+		icon: statisticsIcon,
 	},
 	{
 		path: "/account",
 		name: "Account",
 		component: Account,
 		inNavBar: true,
+		icon: accountIcon,
 	},
 	{
 		path: "/options",
 		name: "Options",
 		component: Options,
 		inNavBar: true,
+		icon: optionsIcon,
 	},
 
 	// Not in nav
